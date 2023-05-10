cacebf3b01b543838c907ca8e301ce7b58f26d25
shbaheer <98651662+shbaheer@users.noreply.github.com>
2023-05-10 15:33:51 +0430

    Add files via upload

---
+++ Pipe-and-filter-Project/controlers/filed.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Pipe_and_filte.Models
+{
+    public class filed
+    {
+
+        public int id { get; set; }
+        public string filedName { get; set; }
+    }
+}
